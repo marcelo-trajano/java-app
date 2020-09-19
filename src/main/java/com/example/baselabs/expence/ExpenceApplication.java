d0162b803360b5431cf3e3733fff4d49f565e710
Marcelo <marcelo.trajano@gmail.com>
2020-09-19 05:46:18 -0300

    upload

---
+++ src/main/java/com/example/baselabs/expence/ExpenceApplication.java
@@ -0,0 +1,13 @@
+package com.example.baselabs.expence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/baselabs/expence/ExpenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baselabs.expence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
